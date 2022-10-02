34abae7f739c6804f2eb116603e3c89cb783aebd
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-02 18:26:53 +0300

    The first commit 02.10.22

---
+++ Classes.py
@@ -0,0 +1,28 @@
+class Dog:
+    """Проста спроба змоделювати собаку"""
+
+    def __init__(self, name, age):
+        """Ініціювати атрибути ім'я та вік"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Симулювати виконання команди 'сидіти'"""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """Симулювати виконання команди 'лежати'"""
+        print(f"{self.name} rolled over.")
+
+
+my_dog = Dog('Willie', 6)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+my_dog.roll_over()
+
+your_dog = Dog("Lucy", 3)
+print(f"Your dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age} years old.")
+your_dog.sit()
