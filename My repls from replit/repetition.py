aefaaeef91f9b7b86bb9812d0ede00eb3f51f86f
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-03 16:23:10 +0300

    The 7 commit 03.10.22. Renamed files from replit.

---
+++ My repls from replit/repetition.py
@@ -0,0 +1,32 @@
+"""
+Програма
+повторення
+Python
+"""
+
+print("Повторення:")  # Комент
+number1, number2 = 1488, 14.88
+number3 = 88888888
+number4 = 14.88888
+number5 = float(number1)
+text1 = "Азов"
+list1 = [1, 2, 3, 4, 5]
+list1[0] = 100
+tuple1 = (10, 20, 30)
+set1 = {1, 2, 3, 4}
+dict1 = {'azov': 14, 'ukraine': 88}
+text2 = '''Слава
+Україні!'''
+print(number1, type(number1))
+print(number2, type(number2))
+print(number3, type(number3))
+print(number4, type(number4))
+print(text1, type(text1))
+print(list1[0], type(list1))
+print(list1[0:])
+print(list1[1:3])
+print(tuple1[0], type(tuple1))
+print(text2)
+print(set1, type(set1))
+print(dict1['azov'], type(dict1))
+print(number5, type(number5))
