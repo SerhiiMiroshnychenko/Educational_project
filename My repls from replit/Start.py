0e707cd9f546cad73ea936b3ecb404446e59a775
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-03 11:21:43 +0300

    The fifth commit 03.10.22. Add a directory 'My repls from replit' and a file 'Start'.

---
+++ My repls from replit/Start.py
@@ -0,0 +1,26 @@
+print('''START
+
+
+       ''')
+a, b, c = 1, 1.5, "text"
+l = [10, 20, 30, 40]
+t = (100, 200, 300)
+print("a = ", a, "type a:", type(a))
+print("b = ", b, "type b:", type(b))
+print("c = ", c, "type c:", type(c))
+l[1] = 200
+print("l = ", l[0:], "type l:", type(l))
+print("t = ", t[1], "type t:", type(t))
+s = {1, 2, 3, 4, 5}
+print("s = ", s, "type s:", type(s))
+d = {'peter': 25, 'alex': 35}
+print("peters age = ", d['peter'], "type d:", type(d))
+f = float(a)
+print("f = ", f, "type f:", type(f))
+v = '''
+
+
+Glory
+to
+Ukraine'''
+print(v)
