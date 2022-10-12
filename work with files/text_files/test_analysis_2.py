f63f5e7d62e1e6d7b28198e2f055ec1ef6105ff8
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-12 16:50:19 +0300

    Modified 1 file.

--- work with files/text_files/test_analysis_2.py
+++ work with files/text_files/test_analysis_2.py
@@ -1,3 +1,17 @@
 line = "Row, row,row your boat"
 print(line.count('row'))
 print(line.lower().count('row'))
+
+filename = 'Alice in Wonderland.txt'
+file_path = f'C:/Users/admin/Desktop/For files/{filename}'
+
+try:
+    with open(file_path, encoding='utf-8') as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Sorry, there is not the file {filename} by address {file_path}.")
+else:
+    word = input("What word to count: ")
+    num_words = contents.lower().count(word)
+    print(f"The file {filename} has about {num_words} words {word}.")
+
