bdf35a86f68e2bb136af04ef867dea62bfc6af2f
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-13 16:43:34 +0300

    The educational project commit. What exactly happens here is not important to anyone but the author. :-)

--- work with files/data storage/remember_me_full_refactoring.py
+++ work with files/data storage/remember_me_full_refactoring.py
@@ -3,7 +3,7 @@ import json
 
 def get_stored_username():
     """Видобути збережене ім'я, якщо таке є."""
-    filename = 'user_name.json'
+    filename = 'user-name.json'
     try:
         with open(filename) as f:
             username = json.load(f)
@@ -16,8 +16,19 @@ def get_stored_username():
 def get_new_username():
     """Запитати ім'я користувача."""
     username = input("What is your name? ")
-    filename = 'user_name.json'
+    filename = 'user-name.json'
     with open(filename, 'w') as f:
         json.dump(username, f)
+    return username
+
+
+def greet_user():
+    """Привітати користувача на ім'я."""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We`ll remember you when you come back, {username}!")
 
 
