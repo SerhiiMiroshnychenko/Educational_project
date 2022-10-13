62112e11f37239cc480a2b1eeebccb926ae5b026
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-13 15:27:09 +0300

    Added 1 file, modified 1 file

--- work with files/data storage/full_remember_me.py
+++ work with files/data storage/full_remember_me.py
@@ -3,5 +3,15 @@ import json
 # Завантажити ім'я користувача, якщо воно вже було збережене.
 # В іншому разі запитати ім'я користувача та збережіть його.
 
-filename = ''
+filename = 'username.json'
 
+try:
+    with open(filename) as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+        print(f"We`ll remember you when you come back, {username}!")
+else:
+    print(f"Welcome back, {username}!")
