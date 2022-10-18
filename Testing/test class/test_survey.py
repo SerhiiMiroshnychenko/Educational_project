fe88583a5d736350d4b23d78baafcd7de3b076fc
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-18 18:51:25 +0300

    The educational project commit. What exactly happens here is not important to anyone but the author. :-)

--- Testing/test class/test_survey.py
+++ Testing/test class/test_survey.py
@@ -1,4 +1,17 @@
 import unittest
 from survey import AnonymousSurvey
 
+
 class TestAnonymousSurvey(unittest.TestCase):
+    """Тести для класу AnonymousSurvey."""
+
+    def test_store_single_response(self):
+        """Перевірити, чи окрема відповідь зберігається правильно."""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        my_survey.store_response('English')
+        self.assertIn('English', my_survey.responses)
+
+
+if __name__ == '__main__':
+    unittest.main()
