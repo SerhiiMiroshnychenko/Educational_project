4552278871d9f67bdd48bb2cf4ca1fb482dbfebd
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-10-18 19:01:51 +0300

    The educational project commit. What exactly happens here is not important to anyone but the author. :-)

--- Testing/test class/test_survey.py
+++ Testing/test class/test_survey.py
@@ -12,6 +12,17 @@ class TestAnonymousSurvey(unittest.TestCase):
         my_survey.store_response('English')
         self.assertIn('English', my_survey.responses)
 
+    def test_store_three_responses(self):
+        """Перевірити, чи три індивідуальних відповіді зберігаються правильно."""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
+
 
 if __name__ == '__main__':
     unittest.main()
