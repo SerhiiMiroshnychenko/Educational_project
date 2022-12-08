b3aaf39ab53dc297c3b617e3421e6ae529a4dedb
Serhii Miroshnychenko <veprzlisu@gmail.com>
2022-12-08 12:09:02 +0200

    homework_1

---
+++ Self_Study/clock/clock.py
@@ -0,0 +1,19 @@
+from cProfile import label
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title('Clock')
+
+def time():
+    string = strftime('%H:%M:%S')
+    label.config(text=string)
+    label.after(1000, time)
+
+
+label = Label(root, font=('DS-Digital', 80), background='black', foreground='cyan')
+label.pack(anchor='center')
+time()
+
+mainloop()
